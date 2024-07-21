1fccd28d08ac4f31f6e49c30938e3ecc1aefb6dd
andrechen77 <101299705+andrechen77@users.noreply.github.com>
2024-07-20 22:20:50 -0400

    pretty print results

--- src/main.rs
+++ src/main.rs
@@ -31,13 +31,13 @@ fn main() -> Result<()> {
         process_jobs(jobs.into_iter());
 
     println!("\nGlobal Tracker: ================");
-    print_job_tracker_results(&global_tracker);
+    println!("{}", format_job_tracker_results(&global_tracker));
     for (rep, tracker) in rep_specific_trackers {
         println!(
             "\nTracker for {}: =================",
             rep.unwrap_or("Unknown Sales Rep".to_owned())
         );
-        print_job_tracker_results(&tracker);
+        println!("{}", format_job_tracker_results(&tracker));
     }
     for (rep, jobs) in invalid_jobs {
         println!(
@@ -106,12 +106,15 @@ fn process_jobs(jobs: impl Iterator<Item = Job>) -> ProcessJobsResult {
     for job in jobs {
         match job.into_analyzed() {
             Ok(analyzed) => {
-                let kind = analyzed.kind.into_int();
-                global_tracker.add_job(kind, &analyzed.timestamps, analyzed.loss_timestamp);
-                rep_specific_trackers
-                    .entry(analyzed.job.sales_rep.clone())
-                    .or_insert_with(build_job_tracker)
-                    .add_job(kind, &analyzed.timestamps, analyzed.loss_timestamp);
+                // only add settled jobs to the trackers
+                if analyzed.is_settled() {
+                    let kind = analyzed.kind.into_int();
+                    global_tracker.add_job(kind, &analyzed.timestamps, analyzed.loss_timestamp);
+                    rep_specific_trackers
+                        .entry(analyzed.job.sales_rep.clone())
+                        .or_insert_with(build_job_tracker)
+                        .add_job(kind, &analyzed.timestamps, analyzed.loss_timestamp);
+                }
             }
             Err((job, err)) => {
                 invalid_jobs.entry(job.sales_rep.clone()).or_insert_with(Vec::new).push((job, err));
@@ -122,7 +125,7 @@ fn process_jobs(jobs: impl Iterator<Item = Job>) -> ProcessJobsResult {
     ProcessJobsResult { global_tracker, rep_specific_trackers, invalid_jobs }
 }
 
-type JobTracker3x5 = JobTracker<{ JobKind::num_variants() }, { Milestone::num_variants() }>;
+type JobTracker3x5 = JobTracker<{ JobKind::NUM_VARIANTS }, { Milestone::NUM_VARIANTS }>;
 
 fn build_job_tracker() -> JobTracker3x5 {
     JobTracker::new([
@@ -132,8 +135,61 @@ fn build_job_tracker() -> JobTracker3x5 {
     ])
 }
 
-fn print_job_tracker_results(tracker: &JobTracker3x5) {
-    todo!()
+fn format_job_tracker_results(tracker: &JobTracker3x5) -> String {
+    let iwc = JobKind::InsuranceWithContingency.into_int(); // "insurance with contingency"
+    let iwo = JobKind::InsuranceWithoutContingency.into_int(); // "insurance without contingency"
+    let ret = JobKind::Retail.into_int(); // "retail"
+
+    let num_appts = tracker.calc_stats(Milestone::AppointmentMade.into_int(), &[iwc, iwo, ret]).num_total;
+    let num_installs = tracker.calc_stats(Milestone::Installed.into_int(), &[iwc, iwo, ret]).num_total;
+    let (num_losses, avg_loss_time) = tracker.calc_stats_of_loss();
+    let CalcStatsResult {
+        num_total: continge_total,
+        conversion_rate: continge_conv,
+        average_time_to_achieve: continge_time,
+    } = tracker.calc_stats(Milestone::ContingencySigned.into_int(), &[iwc]);
+    let CalcStatsResult {
+        num_total: contract_insurance_total,
+        conversion_rate: contract_insurance_conv,
+        average_time_to_achieve: contract_insurance_time,
+    } = tracker.calc_stats(Milestone::ContractSigned.into_int(), &[iwc, iwo]);
+    let CalcStatsResult {
+        num_total: contract_retail_total,
+        conversion_rate: contract_retail_conv,
+        average_time_to_achieve: contract_retail_time,
+    } = tracker.calc_stats(Milestone::ContractSigned.into_int(), &[ret]);
+    let CalcStatsResult {
+        num_total: install_insurance_total,
+        conversion_rate: install_insurance_conv,
+        average_time_to_achieve: install_insurance_time,
+    } = tracker.calc_stats(Milestone::Installed.into_int(), &[iwc, iwo]);
+    let CalcStatsResult {
+        num_total: install_retail_total,
+        conversion_rate: install_retail_conv,
+        average_time_to_achieve: install_retail_time,
+    } = tracker.calc_stats(Milestone::Installed.into_int(), &[ret]);
+
+    fn into_days(time: TimeDelta) -> f64 {
+        const SECONDS_PER_DAY: f64 = 86400.0;
+        time.num_seconds() as f64 / SECONDS_PER_DAY
+    }
+
+    format!(
+        "Appts {} | Installed {} | Lost {}\n\
+        Average Loss Time: {}\n\
+        Contingencies:             Rate {:.2} | Total {:2} | Avg Time (days) {:.2}\n\
+        Contracts (Insurance):     Rate {:.2} | Total {:2} | Avg Time (days) {:.2}\n\
+        Contracts (Retail):        Rate {:.2} | Total {:2} | Avg Time (days) {:.2}\n\
+        Installations (Insurance): Rate {:.2} | Total {:2} | Avg Time (days) {:.2}\n\
+        Installations (Retail):    Rate {:.2} | Total {:2} | Avg Time (days) {:.2}",
+        num_appts, num_installs, num_losses,
+        into_days(avg_loss_time),
+        continge_conv, continge_total, into_days(continge_time),
+        contract_insurance_conv, contract_insurance_total, into_days(contract_insurance_time),
+        contract_retail_conv, contract_retail_total, into_days(contract_retail_time),
+        install_insurance_conv, install_insurance_total, into_days(install_insurance_time),
+        install_retail_conv, install_retail_total, into_days(install_retail_time),
+    )
 }
 
 type Timestamp = DateTime<Utc>;
@@ -148,9 +204,7 @@ enum Milestone {
     Installed,
 }
 impl Milestone {
-    const fn num_variants() -> usize {
-        5
-    }
+    const NUM_VARIANTS: usize = 5;
 
     fn ordered_iter() -> impl Iterator<Item = Self> {
         static ORDERED_VARIANTS: [Milestone; 5] = [
@@ -242,7 +296,7 @@ struct AnalyzedJob {
 
 impl AnalyzedJob {
     fn is_settled(&self) -> bool {
-        self.loss_timestamp.is_some() || self.timestamps.len() == Milestone::num_variants()
+        self.loss_timestamp.is_some() || self.timestamps.len() == Milestone::NUM_VARIANTS
     }
 }
 
@@ -253,9 +307,8 @@ enum JobKind {
     Retail,
 }
 impl JobKind {
-    const fn num_variants() -> usize {
-        3
-    }
+    const NUM_VARIANTS: usize = 3;
+
 
     const fn into_int(self) -> usize {
         match self {
